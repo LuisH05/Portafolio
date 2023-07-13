ae4edd8df51056c58678275606412469ee582a96
Luis Hernandez <lic.luish05@gmail.com>
2023-07-13 11:41:55 -0500

    navbar

--- Portafolio/tailwind.config.js
+++ Portafolio/tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  mode: 'jit',
+  purge: [
+  './src/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  extend: {
+    
+    screens:{
+      'xsm': {'min': '320px', 'max': '427px'},
+      'xxsm':{'min': '427px', 'max': '886px'},
+      'md': {'min': '886px', 'max': '4000px'}
+      
+    },
 
+      colors:{
+      primary: "#222",
+      secundary: "#fff",
+      acento: "#222",
+
+      btn1:'#F9A8D4',
+      btn2:'#F472B6',
+    },
+  
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+    }
+  }
+  },
+  variants: {},
+  plugins: []
+}
