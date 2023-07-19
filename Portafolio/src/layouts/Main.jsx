1387cbfca925d62b7f34674e874140ebc130a0b3
Luis Hernandez <lic.luish05@gmail.com>
2023-07-19 17:40:37 -0500

    HOME

--- Portafolio/src/layouts/Main.jsx
+++ Portafolio/src/layouts/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { Outlet } from 'react-router-dom';
+import Home from '../components/Home';
 
 export default function Main() {
   return (
     <>
     <Navbar/>
+    <Home />
     </>
   )
 }
