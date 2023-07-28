2c5cf0c5cdbb16acb8b1f5706a70831135245b90
Luis Hernandez <lic.luish05@gmail.com>
2023-07-28 18:28:22 -0500

    add page project

--- Portafolio/src/router/router.jsx
+++ Portafolio/src/router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from '../layouts/Main.jsx'
 import About from "../Pages/About.jsx";
 import Home from "../App.jsx";
+import Project from "../Pages/Project.jsx";
 
 
 const routers = createBrowserRouter([
@@ -10,6 +11,7 @@ const routers = createBrowserRouter([
         children:[
             {path: '/', element: <Home />},
             {path: '/about', element: <About />},
+            {path: '/project', element: <Project />},
         ]
     }
 ])
