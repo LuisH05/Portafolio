ba9181c06e0f9ac7d024df8d50c817f17a48e7ac
Luis Hernandez <lic.luish05@gmail.com>
2023-08-05 09:35:35 -0500

    tecnologias

--- Portafolio/src/Pages/Tecnologias.jsx
+++ Portafolio/src/Pages/Tecnologias.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Tecnologias() {
   return (
-    <div>Tecnologias</div>
+    <>
+      <h1 className="flex justify-center mt-10 text-4xl font-bold tracking-tight text-[#03CC90] xsm:text-5xl">
+          Tecnologias
+      </h1>
+    </>
   )
 }
