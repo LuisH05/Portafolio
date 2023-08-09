baefbee6982d32f8a6bdfd8eaadb25bb25e33f9b
Luis Hernandez <lic.luish05@gmail.com>
2023-08-09 09:27:39 -0500

    tecnologias

--- Portafolio/src/Pages/Tecnologias.jsx
+++ Portafolio/src/Pages/Tecnologias.jsx
@@ -6,6 +6,12 @@ export default function Tecnologias() {
       <h1 className="flex justify-center mt-10 text-4xl font-bold tracking-tight text-[#03CC90] xsm:text-5xl">
           Tecnologias
       </h1>
+      <div>
+        <h3 className='flex justify-center text-lg font-bold tracking-tight text-[#FFFFFF] xsm:text-5xl mt-20'>
+          Front-End
+        </h3>
+        
+      </div>
     </>
   )
 }
